e6c9c354414be2b4ccfa617b9987738f67c04838
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2024-09-25 21:26:45 +0530

    Chatbot Feature

--- src/components/Appoinments/ViewRecords.js
+++ src/components/Appoinments/ViewRecords.js
@@ -4,7 +4,36 @@ import './ViewRecords.css';
 const ViewRecords = () => {
   return (
     <div className="fullscreen-image">
-        {/* <img src='images/ViewRecords/img1.jpg'></img> */}
+        <div className='charts'>
+        <div class="flex flex-wrap">
+  
+  <div class="w-1/2 p-4 ml-">
+    <div class="bg-gray-200 h-32 flex items-center justify-center">
+      Row 1, Column 1
+    </div>
+    <div class="bg-gray-300 h-32 flex items-center justify-center">
+      Row 2, Column 1
+    </div>
+    <div class="bg-gray-400 h-32 flex items-center justify-center">
+      Row 3, Column 1
+    </div>
+  </div>
+
+  
+  <div class="w-1/2 p-4">
+    <div class="bg-gray-200 h-32 flex items-center justify-center">
+      Row 1, Column 2
+    </div>
+    <div class="bg-gray-300 h-32 flex items-center justify-center">
+      Row 2, Column 2
+    </div>
+    <div class="bg-gray-400 h-32 flex items-center justify-center">
+      Row 3, Column 2
+    </div>
+  </div>
+</div>
+
+        </div>
     </div>
   );
 }

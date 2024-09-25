0340b322366c8d9f546e4ef41b077ab38b1c8b9b
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2024-09-26 01:07:05 +0530

    new features add

--- backend/signup&appoinment/controller/userController.js
+++ backend/signup&appoinment/controller/userController.js
@@ -2,6 +2,7 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { Message } from "../models/messageSchema.js";
 import { User } from "../models/userSchema.js";
+import {generateToken} from "../utils/jwtToken.js";
 
 export const patientRegister = catchAsyncErrors(async(req, res, next) =>{
     const {
@@ -40,8 +41,30 @@ export const patientRegister = catchAsyncErrors(async(req, res, next) =>{
         gender, 
         role
     });
-    res.status(200).json({
-        success: true,
-        message: "User Registered!", 
-    });
+    generateToken(user, "User Registered!", 200, res);
+});
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password, confirmPassword, role } = req.body;
+    if (!email || !password || !confirmPassword || !role) {
+        return next(new ErrorHandler("Please provide All Details!", 400));
+    }
+    if (password !== confirmPassword) {
+        return next(
+            new ErrorHandler("Password & Confirm Password Do Not Match!", 400)
+        );
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+        return next(new ErrorHandler("Invalid Email Or Password!", 400));
+    }
+  
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid Email Or Password!", 400));
+    }
+    if (role !== user.role) {
+        return next(new ErrorHandler(`User Not Found With This Role!`, 400));
+    }
+    generateToken(user, "User Login Successfully!", 200, res);
 });
--- backend/signup&appoinment/router/userRouter.js
+++ backend/signup&appoinment/router/userRouter.js
@@ -3,6 +3,7 @@ import { patientRegister } from "../controller/userController.js";
 
 const router = express.Router();
 
-router.post("/patient/register", patientRegister)
+router.post("/patient/register", patientRegister);
+router.post("/login", login);
 
 export default router;
